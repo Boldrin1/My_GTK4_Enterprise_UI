db049e5523f30dd93fb2b85b6654a6621123d514
Boldrin1 <srzip74@gmail.com>
2025-07-07 10:20:09 -0300

    07/07

--- src/main.c
+++ src/main.c
@@ -6,19 +6,26 @@ static void text(GtkWidget *widget,gpointer data){
 
 static void on_activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window = gtk_application_window_new(app);
-    gtk_window_set_title(GTK_WINDOW(window), "Minha Janela GTK4");
-    gtk_window_set_default_size(GTK_WINDOW(window), 400, 300);
+    gtk_window_set_title(GTK_WINDOW(window), "ContabilidadeOne");
+    gtk_window_maximize(GTK_WINDOW(window));
     gtk_window_present(GTK_WINDOW(window));
 
 
     GtkWidget *button;
+    GtkWidget *box;
+
     button = gtk_button_new_with_label("Click");
     g_signal_connect(button,"clicked",G_CALLBACK(text),NULL);
     
-    gtk_window_set_child(GTK_WINDOW(window), button);
+    box = gtk_box_new(GTK_ORIENTATION_VERTICAL,0);
+    gtk_widget_set_halign(box,GTK_ALIGN_CENTER);
+
+    gtk_box_append(GTK_BOX(box),button);
+    gtk_window_set_child(GTK_WINDOW(window), box);
     gtk_window_present(GTK_WINDOW(window));
 
 }
+
 
 int main(int argc, char *argv[]){
     GtkApplication *app = gtk_application_new("com.contabilidade.app", G_APPLICATION_DEFAULT_FLAGS);
