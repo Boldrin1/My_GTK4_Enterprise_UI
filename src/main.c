576cdc3b1e240e3f1a454953822788fb8e31b33d
Boldrin1 <srzip74@gmail.com>
2025-07-11 09:44:07 -0300

    botao de add and remove

--- src/main.c
+++ src/main.c
@@ -1,42 +1,48 @@
 #include <gtk/gtk.h>
 
-static void adicionar(GtkWidget *widget,gpointer data){
-	int i;
-	i = i+1;
-	int *ptr;
-	ptr = &i;
+int total = 0;
+GtkWidget *label;
+
+static void atualizar_label(){
+    gchar *texto = g_strdup_printf("O valor total é: %d", total);
+    gtk_label_set_text(GTK_LABEL(label), texto);
+    g_free(texto);
 }
 
-static void remover(GtkWidget *widget,gpointer data){
-	int x;
-	x= x+1;
-	int *ptr2;
-	ptr2 = &x;
+static void adicionar(GtkWidget *widget, gpointer data) {
+    total += 1;
+    atualizar_label();
+}
+
+static void remover(GtkWidget *widget, gpointer data) {
+    total -= 1;
+    atualizar_label();
 }
 
 
 static void on_activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
-    GtkWidget *button;
-    GtkWidget *label;
+    GtkWidget *button_add;
+    GtkWidget *button_remove;
     GtkWidget *grid;
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window),"ContabilidadeOne");
     gtk_window_maximize(GTK_WINDOW(window));
 
-    grid = gyk_grid_new();
+    grid = gtk_grid_new();
     gtk_window_set_child(GTK_WINDOW(window),grid);
 
+    label = gtk_label_new("O valor total é: 0");
+    gtk_grid_attach(GTK_GRID(grid),label,0,0,1,1);
 
-    button = gtk_button_new_with_label("Adicionar");
-    g_signal_connect(button,"activate",G_CALLBACK(adicionar),NULL);
-    gtk_grid_attach(GTK_GRID(grid),button,0,0,1,1);
-
+    button_add = gtk_button_new_with_label("Adicionar");
+    g_signal_connect(button_add,"clicked",G_CALLBACK(adicionar),NULL);
+    gtk_grid_attach(GTK_GRID(grid),button_add,0,1,1,1);
     
-    button = gtk_button_new_with_label("Excluir");
-    g_signal_connect(button,"activate",G_CALLBACK(button,"activate",G_CALLBACK(remover));
-    gtk_grid_attach(GTK_GRID(grid),button,0,0,1,1);
+    button_remove = gtk_button_new_with_label("Excluir");
+    g_signal_connect(button_remove,"clicked",G_CALLBACK(remover),NULL);
+    gtk_grid_attach(GTK_GRID(grid),button_remove,1,1,1,1);
 
 
     gtk_window_present(GTK_WINDOW(window));
